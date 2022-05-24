9d7b20f9a89843c6986b364d523706fd0f9e022b
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-05-24 20:09:27 +0000

    chore(deps)!: upgrade provider project

--- src/data-external.ts
+++ src/data-external.ts
@@ -8,6 +8,13 @@ import * as cdktf from 'cdktf';
 
 export interface DataExternalConfig extends cdktf.TerraformMetaArguments {
   /**
+  * Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/external/d/external#id DataExternal#id}
+  *
+  * Please be aware that the id field is automatically added to all resources in Terraform providers using a Terraform provider SDK version below 2.
+  * If you experience problems setting this value it might not be settable. Please take a look at the provider documentation to ensure it should be settable.
+  */
+  readonly id?: string;
+  /**
   * A list of strings, whose first element is the program to run and whose subsequent elements are optional command line arguments to the program. Terraform does not execute the program through a shell, so it is not necessary to escape shell metacharacters nor add quotes around arguments containing spaces.
   * 
   * Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/external/d/external#program DataExternal#program}
@@ -61,6 +68,7 @@ export class DataExternal extends cdktf.TerraformDataSource {
       count: config.count,
       lifecycle: config.lifecycle
     });
+    this._id = config.id;
     this._program = config.program;
     this._query = config.query;
     this._workingDir = config.workingDir;
@@ -71,8 +79,19 @@ export class DataExternal extends cdktf.TerraformDataSource {
   // ==========
 
   // id - computed: true, optional: true, required: false
+  private _id?: string; 
   public get id() {
     return this.getStringAttribute('id');
+  }
+  public set id(value: string) {
+    this._id = value;
+  }
+  public resetId() {
+    this._id = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get idInput() {
+    return this._id;
   }
 
   // program - computed: false, optional: false, required: true
@@ -105,8 +124,9 @@ export class DataExternal extends cdktf.TerraformDataSource {
   }
 
   // result - computed: true, optional: false, required: false
-  public result(key: string): string | cdktf.IResolvable {
-    return new cdktf.StringMap(this, 'result').lookup(key);
+  private _result = new cdktf.StringMap(this, "result");
+  public get result() {
+    return this._result;
   }
 
   // working_dir - computed: false, optional: true, required: false
@@ -131,6 +151,7 @@ export class DataExternal extends cdktf.TerraformDataSource {
 
   protected synthesizeAttributes(): { [name: string]: any } {
     return {
+      id: cdktf.stringToTerraform(this._id),
       program: cdktf.listMapper(cdktf.stringToTerraform)(this._program),
       query: cdktf.hashMapper(cdktf.stringToTerraform)(this._query),
       working_dir: cdktf.stringToTerraform(this._workingDir),
